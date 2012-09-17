c1aaab54be17a8fa758fffd6f58e6145851ba3a0
kubintsev <kubintsev@kubintsev.(none)>
2012-09-17 09:52:53 +0400

    fix in view class

--- libs/router.php
+++ libs/router.php
@@ -1,117 +1,110 @@
 <?php
-
-Class Router {
-
-        private $path;
-        private static $controller;
-        private static $args;
-
-        function __construct() {
-            $this->setPath(COMPONENTS);
-        }
-        
-        private function deSlash($str)
-        {
-            return trim($str, '/\\');
-        }
-        
-        /**
-         * Устанавливает каталог компонентов
-         * @param string $path
-         * @throws Exception
-         */
-        private function setPath($path) 
-        {
-            $this->path = $path;
-
-            if (is_dir($path) == false) {
-                    throw new Exception ('Invalid controller path: `' . $path . '`');
-            }
-            
-            $this->path = $path;
-        }
-        
-        /**
-         * Метод обработки URL для маршрутизации
-         * Возвращает имя контроллера и переданные ему параметры
-         */
-        private function getController() 
-        {
-            // Считывание запроса URL
-            $route = (empty($_SERVER["REQUEST_URI"])) ? '' : $_SERVER["REQUEST_URI"];
-            
-            // Вычленяем префикс URL (корневую папку приложения)
-            if ( $route )
-                $route = substr( $route, strlen(URLROOT) );
-            
-            // Предотвращение дублирования главной страницы: редирект с index.php на /
-            $mainpage = array('index.php', 'index', 'index/');
-            
-            if ( in_array($route, $mainpage) )
-                $this->redirect();
-            
-            if (empty($route)) 
-                $route = 'index';
-            else
-                $route = $this->deSlash($route); // Получаем раздельные части
-            
-            // Удаляем расширение файла и параметры из запроса, если кто-то додумался их передавать после "?"
-            $route = preg_replace('#(\?.*)?$#', '', $route);
-            
-            
-            /* Основная логика маршрутизатора 
-             * Например отфильтрованный URL = 'index/ru/mow', 
-             * тогда index - контроллер, ru и mow - параметры для контроллера
-             */
-            $parts = explode('/', $route);
-
-            
-            // 'index/ru/mow'
-            if ( is_array($parts) )
-            {
-                $controller = array_shift($parts);
-                $args = $parts;
-            }
-            else
-                $controller = $route;
-            
-            self::$controller = $controller;
-            self::$args = $args;
-    }
-   
-    /*
-     * Нахождение контроллера и передача управления ему
+/**
+ * Description of View
+ *
+ * @author kubintsev
+ */
+class View {
+    private $component_path;
+    private $global_template;
+    private $template;
+    private $vars;
+    
+    /**
+     * 
+     * @param string $tmpl the name of global template
      */
-    function delegate() 
+    function __construct($tmpl = null) 
     {
-        // Анализируем URL
-        $this->getController();
-
-        // Создаём экземпляр контроллера
-        $class = 'Controller_' . self::$controller;
-        
-        $controller = $this->path.DS.self::$controller.DS.self::$controller.'.php';
-        
-        // Файл контроллера доступен?
-        if (is_readable($controller) == false) {
-            if (DEBUG)
-                throw new Exception('Controller "'.$controller.'" not found');
-            else
-                $this->redirect();
-        }
-        
-        // Подключаем контроллер
-        require_once ($controller);
-        
-        // Делегируем управление контроллеру
-        $obj = new $class(self::$controller);
-        $obj->Run(self::$args);
+        if ( $tmpl === null)
+            $this->global_template = 'global_tmpl.php';
+        else
+            $this->global_template = $tmpl;
+            
     }
 
-    function redirect($url = '')
+    /**
+     * Sets new global template
+     * @param string $path
+     */
+    function setGlobalTemplate($tmpl)
     {
-        header("HTTP/1.1 301 Moved Permanently");
-        header('Location: http://'.$_SERVER['HTTP_HOST'].URLROOT.$url );
-        exit();
+        $this->global_template = strtolower($tmpl);
+    }
+    
+    /**
+     * Sets component path
+     * @param string $path
+     */
+    function setPath($path)
+    {
+        $this->component_path = $path;
+    }
+    
+    /**
+     * Sets template vars
+     * @param array $vars
+     * @return \View
+     */
+    function loadVars(array $vars)
+    {
+        $this->vars = $vars;
+        return $this;
+    }
+    
+    /**
+     * Sets template name
+     * @param string $tmpl
+     * @return \View
+     */
+    function loadTemplate($tmpl)
+    {
+        $this->template = strtolower($tmpl);
+        
+        return $this;
+    }
+    
+    /**
+     * Render the view. If $is_ajax = true then render goes without global template
+     * @param bool $is_ajax 
+     * @throws Exception
+     */
+    function render($is_ajax = false)
+    {
+        try {
+            
+            $template = $this->component_path.'view_'.$this->template.'.php';
+
+            if ( !file_exists($template) )
+            {
+                throw new Exception('"'.$template.'" not found!');
+            }
+            
+            extract($this->vars);
+
+            if (DEBUG) {
+                DEBUG::log(DEBUG::getmem());
+            }
+            
+            $debug = DEBUG::getlog();
+            
+            ob_start();
+            require $template;
+            
+            $content = ob_get_contents();
+            ob_end_clean();
+        }
+        catch (Exception $e)
+        {
+            Debug::log('VIEW CLASS error!<br>Line:'.$e->getLine().'<br>Message: '.$e->getMessage().'<br>');
+        }
+        
+        header('Content-Type: text/html; charset='.CODEPAGE);
+        header('P3P: CP="CAO PSA OUR"');
+        
+        if (!$is_ajax)
+            require_once GLOBALVIEWS.$this->global_template;
+        else
+            echo $content;
     }
 }
